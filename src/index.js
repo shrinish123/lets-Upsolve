020642879b4efa35a38047d16085a71c5977a6b1
Mayank Kashyap <mayankkashyap7970@gmail.com>
2022-12-15 19:15:44 +0530

    Adding Error Handler

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-   <BrowserRouter>
-	  <App />  
+  <BrowserRouter>
+    <App />
+    <ToastContainer autoClose={2000} theme={"colored"} icon={false} />
   </BrowserRouter>
   // </React.StrictMode>
 );
