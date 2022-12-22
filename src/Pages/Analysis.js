0a1026564c22aab511e09d57eadb82ed69b23fdf
Mayank Kashyap <mayankkashyap7970@gmail.com>
2022-12-23 02:00:02 +0530

    Enhanced Table moved to another Component

--- src/Pages/Analysis.js
+++ src/Pages/Analysis.js
@@ -1,169 +1,149 @@
-import React,{useState,useEffect} from 'react'
-// import CanvasJSReact from '../assets/canvasjs.react';
-import axios from 'axios'
-import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Unstable_Grid2';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Unstable_Grid2";
 import FusionCharts from "fusioncharts";
 import charts from "fusioncharts/fusioncharts.charts";
 import ReactFusioncharts from "react-fusioncharts";
 
-// const {CanvasJS,CanvasJSChart} = CanvasJSReact;
 charts(FusionCharts);
 
-
 const Analysis = () => {
-  
-    const user = JSON.parse(localStorage.getItem('user'));
-    const [graphData,setGraphData] = useState({});
-   
-    useEffect(()=>{
+  const user = JSON.parse(localStorage.getItem("user"));
+  const [graphData, setGraphData] = useState({});
 
-        const getSubmissions =async() => {
+  useEffect(() => {
+    const getSubmissions = async () => {
+      try {
+        const submissionsres = await axios.get(
+          `https://codeforces.com/api/user.status?handle=${user.handle}&from=1`
+        );
 
-            try{
-                const submissionsres = await axios.get(`https://codeforces.com/api/user.status?handle=${user.handle}&from=1`);
-               
-                const submissions = submissionsres.data.result;
-                const tagsMap = new Map();
-                const tags = [];
-                // let untaggedSubmissions = 0;
-    
-                const verdictsMap = new Map();
-                const verdicts = [];
-                
-                for(let submission of submissions){
-    
-                    if(!submission.problem.tags[0]){
-                      
-                    }
-                    if(submission.problem.tags[0]){
-                        
-                        if(!tagsMap.has(submission.problem.tags[0])){
-                            tagsMap.set(submission.problem.tags[0],1);
-    
-                        }else{
-                            let count = tagsMap.get(submission.problem.tags[0]);
-                            count++;
-                            tagsMap.set(submission.problem.tags[0],count);
-                        }
-                        
-                    }
-    
-                    if(submission.verdict){
-                       
-                        if(!verdictsMap.has(submission.verdict)){
-                            verdictsMap.set(submission.verdict,1);
-                        }else{
-                          let count = verdictsMap.get(submission.verdict);
-                          count++;
-                          verdictsMap.set(submission.verdict,count);
-                        }
-                    }
-                    
-                }
-    
-                tagsMap.forEach((count,tag)=> {
-                    
-                    const tagObj = {
-    
-                        label: tag,
-                        value:count,
-                    }
-                     tags.push(tagObj);
-                })
-                console.log(verdictsMap);
-    
-                verdictsMap.forEach((count,verdict)=> {
-                    
-                    const verdictObj = {
-    
-                        label: verdict,
-                        value:count,
-                    }
-                     verdicts.push(verdictObj);
-                })
-                console.log(verdicts)
-    
-               setGraphData({tagData:tags,verdictData:verdicts});
+        const submissions = submissionsres.data.result;
+        const tagsMap = new Map();
+        const tags = [];
+        // let untaggedSubmissions = 0;
+
+        const verdictsMap = new Map();
+        const verdicts = [];
+
+        for (let submission of submissions) {
+          // if (!submission.problem.tags[0]) {
+          // }
+          if (submission.problem.tags[0]) {
+            if (!tagsMap.has(submission.problem.tags[0])) {
+              tagsMap.set(submission.problem.tags[0], 1);
+            } else {
+              let count = tagsMap.get(submission.problem.tags[0]);
+              count++;
+              tagsMap.set(submission.problem.tags[0], count);
             }
-            catch(err){
-               console.log(err);
+          }
+
+          if (submission.verdict) {
+            if (!verdictsMap.has(submission.verdict)) {
+              verdictsMap.set(submission.verdict, 1);
+            } else {
+              let count = verdictsMap.get(submission.verdict);
+              count++;
+              verdictsMap.set(submission.verdict, count);
             }
+          }
         }
 
-      getSubmissions();
-    },[user.handle]);
+        tagsMap.forEach((count, tag) => {
+          const tagObj = {
+            label: tag,
+            value: count,
+          };
+          tags.push(tagObj);
+        });
+        console.log(verdictsMap);
 
-    
+        verdictsMap.forEach((count, verdict) => {
+          const verdictObj = {
+            label: verdict,
+            value: count,
+          };
+          verdicts.push(verdictObj);
+        });
+        console.log(verdicts);
 
-    console.log(graphData.verdictData);
+        setGraphData({ tagData: tags, verdictData: verdicts });
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
-    const dataSource = {
-        chart: {
-          caption: `Tags Solved By ${user.handle}`,
-          subcaption: "",
-          showvalues: "1",
-          showpercentintooltip: "1",
-          numberprefix: "",
-          enablemultislicing: "1",
-          theme: "candy",
-        },
-        data: graphData.tagData
-      };
+    getSubmissions();
+  }, [user.handle]);
 
-      const verdictDataSource = {
-        chart: {
-          caption: `Verdict Distribution ${user.handle}`,
-          subcaption: "",
-          showvalues: "1",
-          showpercentintooltip: "1",
-          numberprefix: "",
-          enablemultislicing: "1",
-          theme: "candy",
-        },
-        data: graphData.verdictData
-      };
-      
-   
-    // const options = {
-    //     animationEnabled: true,
-    //     exportEnabled: true,
-    //     theme: "dark1", // "light1", "dark1", "dark2"
-    //     title:{
-    //         text: `Tags Solved By ${user.handle}`
-    //     },
-    //     data: [{
-    //         type: "pie",
-    //         showInLegend: true,
-	// 		legendText: "{label}",
-	// 		toolTipContent: "{label}: <strong>{y}%</strong>",
-	// 		indexLabel: "{y}%",
-	// 		indexLabelPlacement: "inside",
-    //         dataPoints: graphData.tagData
-    //     }]
-    // }
+  console.log(graphData.verdictData);
 
-    const LargeItem = styled(Paper)(() => ({
-        backgroundColor: '#1A2027',
-        padding: '2rem',
-        textAlign: 'center',
-        margin: '0 15rem',
-        boxShadow:'3px 3px 3px 2px rgba(0, 0, 0, 0.2)'
-      }));
-    
-      const SmallItem = styled(Paper)(() => ({
-        backgroundColor: '#1A2027',
-        padding: '2rem',
-        textAlign: 'center',
-        margin: '0 15rem',
-        boxShadow:'3px 3px 3px 2px rgba(0, 0, 0, 0.2)'
-      }));  
+  const dataSource = {
+    chart: {
+      caption: `Tags Solved By ${user.handle}`,
+      subcaption: "",
+      showvalues: "1",
+      showpercentintooltip: "1",
+      numberprefix: "",
+      enablemultislicing: "1",
+      theme: "candy",
+    },
+    data: graphData.tagData,
+  };
+
+  const verdictDataSource = {
+    chart: {
+      caption: `Verdict Distribution ${user.handle}`,
+      subcaption: "",
+      showvalues: "1",
+      showpercentintooltip: "1",
+      numberprefix: "",
+      enablemultislicing: "1",
+      theme: "candy",
+    },
+    data: graphData.verdictData,
+  };
+
+  // const options = {
+  //     animationEnabled: true,
+  //     exportEnabled: true,
+  //     theme: "dark1", // "light1", "dark1", "dark2"
+  //     title:{
+  //         text: `Tags Solved By ${user.handle}`
+  //     },
+  //     data: [{
+  //         type: "pie",
+  //         showInLegend: true,
+  // 		legendText: "{label}",
+  // 		toolTipContent: "{label}: <strong>{y}%</strong>",
+  // 		indexLabel: "{y}%",
+  // 		indexLabelPlacement: "inside",
+  //         dataPoints: graphData.tagData
+  //     }]
+  // }
+
+  const LargeItem = styled(Paper)(() => ({
+    backgroundColor: "#1A2027",
+    padding: "2rem",
+    textAlign: "center",
+    margin: "0 15rem",
+    boxShadow: "3px 3px 3px 2px rgba(0, 0, 0, 0.2)",
+  }));
+
+  const SmallItem = styled(Paper)(() => ({
+    backgroundColor: "#1A2027",
+    padding: "2rem",
+    textAlign: "center",
+    margin: "0 15rem",
+    boxShadow: "3px 3px 3px 2px rgba(0, 0, 0, 0.2)",
+  }));
 
   return (
     <>
-     <Grid container spacing={2}>
+      <Grid container spacing={2}>
         {/* <Grid xs={12}>
           <LargeItem>
           <CanvasJSChart options = {options} />
@@ -171,32 +151,30 @@ const Analysis = () => {
         </Grid> */}
         <Grid xs={12}>
           <LargeItem>
-          <ReactFusioncharts
-            type="pie3d"
-            width="80%"
-            height="80%"
-            dataFormat="JSON"
-            dataSource={dataSource}
-        />
+            <ReactFusioncharts
+              type="pie3d"
+              width="80%"
+              height="80%"
+              dataFormat="JSON"
+              dataSource={dataSource}
+            />
           </LargeItem>
         </Grid>
         <Grid xs={12}>
-        <SmallItem>
-          <ReactFusioncharts
-            type="pie3d"
-            width="80%"
-            height="25%"
-            dataFormat="JSON"
-            dataSource={verdictDataSource}
-        />
+          <SmallItem>
+            <ReactFusioncharts
+              type="pie3d"
+              width="80%"
+              height="25%"
+              dataFormat="JSON"
+              dataSource={verdictDataSource}
+            />
           </SmallItem>
-      </Grid>
-      <Grid xs={4}>
-        
-      </Grid>
+        </Grid>
+        <Grid xs={4}></Grid>
       </Grid>
     </>
-  )
-}
+  );
+};
 
-export default Analysis
+export default Analysis;
--- src/Pages/Practice.js
+++ src/Pages/Practice.js
@@ -1,94 +1,103 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-import axios from 'axios';
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import axios from "axios";
 
-import { EnhancedTable} from './Tags';
+import EnhancedTable from "../Components/EnhancedTable";
 
 const Practice = () => {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const { rating } = user;
 
+  const [level, setLevel] = useState(1);
+  const [problems, setProblems] = useState([]);
 
-  const user = JSON.parse(localStorage.getItem('user'));
-  const {rating} = user;
-
-  const [level,setLevel] = useState(1);
-  const [problems,setProblems] = useState([]);
-
-  //level1 -- > rating%100 >  +100 
+  //level1 -- > rating%100 >  +100
   // level2 > +200
   //level3 > +300
 
   const Item = styled(Paper)(() => ({
-    backgroundColor: '#1A2027',
-    padding: '2rem',
-    color: 'white',
-    margin:'2rem',
-    cursor: 'pointer',
-    textAlign:'center'
+    backgroundColor: "#1A2027",
+    padding: "2rem",
+    color: "white",
+    margin: "2rem",
+    cursor: "pointer",
+    textAlign: "center",
   }));
 
   const TableItem = styled(Paper)(() => ({
-    padding: '2rem',
-    margin:'2rem',
+    padding: "2rem",
+    margin: "2rem",
   }));
 
-
-  useEffect(()=> {
-
-    const getProblems = async() => {
-
-      try{
-  
-        const startRange = (rating - rating%100) + (level-1)*100;
+  useEffect(() => {
+    const getProblems = async () => {
+      try {
+        const startRange = rating - (rating % 100) + (level - 1) * 100;
         const endRange = startRange + 100;
-  
-        const problemsres = await axios.get(`https://codeforces.com/api/problemset.problems?tags=`);
+
+        const problemsres = await axios.get(
+          `https://codeforces.com/api/problemset.problems?tags=`
+        );
         let problemsArr = problemsres.data.result.problems;
-        problemsArr = problemsArr.filter((problem)=> problem.rating >= startRange && problem.rating <= endRange);
-     
+        problemsArr = problemsArr.filter(
+          (problem) =>
+            problem.rating >= startRange && problem.rating <= endRange
+        );
+
         setProblems(problemsArr);
-        
-  
-      }catch(err){
-         console.log(err);
+      } catch (err) {
+        console.log(err);
       }
-    }
+    };
 
-     getProblems();
-  },[level,rating]);
-
-  
- 
-  
+    getProblems();
+  }, [level, rating]);
 
   return (
     <>
-     <Box >
-      <Grid container spacing={2}>
-        <Grid item xs={4}>
-          <Item onClick={()=> {setLevel(1)}}>Level 1 </Item>
-        </Grid>
-        <Grid item xs={4}>
-          <Item onClick={()=> {setLevel(2)}}>Level 2</Item>
-        </Grid>
-        <Grid item xs={4}>
-          <Item onClick={()=> {setLevel(3)}}>Level 3</Item>
-        </Grid>
-        
-        <Grid item xs={12}>
-          <TableItem>
-            <EnhancedTable problems={problems}/>
-          </TableItem>
-        </Grid>
+      <Box>
+        <Grid container spacing={2}>
+          <Grid item xs={4}>
+            <Item
+              onClick={() => {
+                setLevel(1);
+              }}
+            >
+              Level 1{" "}
+            </Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item
+              onClick={() => {
+                setLevel(2);
+              }}
+            >
+              Level 2
+            </Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item
+              onClick={() => {
+                setLevel(3);
+              }}
+            >
+              Level 3
+            </Item>
+          </Grid>
 
-      </Grid>
-    </Box>
-
+          <Grid item xs={12}>
+            <TableItem>
+              <EnhancedTable problems={problems} />
+            </TableItem>
+          </Grid>
+        </Grid>
+      </Box>
     </>
-  )
-}
+  );
+};
 
-export default Practice
+export default Practice;
